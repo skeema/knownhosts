02fe81dd235b731bcb12f6c17810c37b94080933
Evan Elias <evan@skeema.net>
2022-06-18 00:12:10 -0400

    docs: fix function name/signature in example_test.go
    In order for examples to show up in Go docs, function name must begin with
    Example and have no args or returns.

--- example_test.go
+++ example_test.go
@@ -1,20 +1,27 @@
 package knownhosts_test
 
 import (
+	"log"
+
 	"github.com/skeema/knownhosts"
 	"golang.org/x/crypto/ssh"
 )
 
-func sshConfigForHost(hostWithPort string) (*ssh.ClientConfig, error) {
+func ExampleNew() {
+	sshHost := "yourserver.com:22"
 	kh, err := knownhosts.New("/home/myuser/.ssh/known_hosts")
 	if err != nil {
-		return nil, err
+		log.Fatal("Failed to read known_hosts: ", err)
 	}
 	config := &ssh.ClientConfig{
 		User:              "myuser",
 		Auth:              []ssh.AuthMethod{ /* ... */ },
 		HostKeyCallback:   kh.HostKeyCallback(), // or, equivalently, use ssh.HostKeyCallback(kh)
-		HostKeyAlgorithms: kh.HostKeyAlgorithms(hostWithPort),
+		HostKeyAlgorithms: kh.HostKeyAlgorithms(sshHost),
 	}
-	return config, nil
+	client, err := ssh.Dial("tcp", sshHost, config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+	defer client.Close()
 }
